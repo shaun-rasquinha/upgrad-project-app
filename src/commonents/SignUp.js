2b0e1e44b00534ae788c12bca67e0922fc5517ca
Shaun <shaunrasquinha@gmail.com>
2024-06-26 21:21:26 +0530

    Created Signup Component

---
+++ src/commonents/SignUp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { signUp } from '../api';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await signUp(formData);
+      console.log('User signed up:', response.data);
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="Name" onChange={handleChange} />
+      <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUp;
