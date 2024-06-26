fba4e9e19f088f3a42ace961331a3ba8a0773110
Shaun <shaunrasquinha@gmail.com>
2024-06-26 21:22:03 +0530

    Created Signin Component

---
+++ src/commonents/SignIn.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { signIn } from '../api';
+import { useHistory } from 'react-router-dom';
+
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await signIn(formData);
+      localStorage.setItem('user', JSON.stringify(response.data));
+      history.push('/');
+    } catch (error) {
+      console.error('Error signing in:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+      <button type="submit">Sign In</button>
+    </form>
+  );
+};
+
+export default SignIn;
